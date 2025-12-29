e2f5c71d5ebe73bd38713a769874414dd22657fd
intakhabchy <intakhab.chy@gmail.com>
2025-12-30 01:35:34 +0600

    api tested with token

--- routes/api.php
+++ routes/api.php
@@ -28,3 +28,20 @@ Route::post('/api-token-login', function(Request $request) {
         'token' => $token
     ]);
 });
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/categories', [App\Http\Controllers\CategoryController::class, 'index'])->name('categories.index')->middleware('auth:sanctum');
+    Route::post('/categories', [App\Http\Controllers\CategoryController::class, 'store'])->name('categories.store');
+
+    Route::get('/categorytypes', [App\Http\Controllers\CategoryTypeController::class, 'index'])->name('categorytypes.index');
+    
+    Route::get('/currencies', [App\Http\Controllers\CurrencyController::class, 'index'])->name('currencies.index');
+
+    Route::get('/wallets', [App\Http\Controllers\WalletController::class, 'index'])->name('wallets.index');
+    Route::post('/wallets', [App\Http\Controllers\WalletController::class, 'store'])->name('wallets.store');
+    Route::get('/walletByUser/{user_id}', [App\Http\Controllers\WalletController::class, 'walletByUser'])->name('walletByUser');
+
+    Route::get('/costs', [App\Http\Controllers\CostController::class, 'index'])->name('costs.index');
+    Route::post('/costs', [App\Http\Controllers\CostController::class, 'store'])->name('costs.store');
+});
+
--- routes/web.php
+++ routes/web.php
@@ -8,17 +8,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/categories', [App\Http\Controllers\CategoryController::class, 'index'])->name('categories.index')->middleware('auth:sanctum');
-Route::post('/categories', [App\Http\Controllers\CategoryController::class, 'store'])->name('categories.store');
-
-Route::get('/categorytypes', [App\Http\Controllers\CategoryTypeController::class, 'index'])->name('categorytypes.index');
-Route::get('/currencies', [App\Http\Controllers\CurrencyController::class, 'index'])->name('currencies.index');
-
-Route::get('/wallets', [App\Http\Controllers\WalletController::class, 'index'])->name('wallets.index');
-Route::post('/wallets', [App\Http\Controllers\WalletController::class, 'store'])->name('wallets.store');
-Route::get('/walletByUser/{user_id}', [App\Http\Controllers\WalletController::class, 'walletByUser'])->name('walletByUser');
-
-Route::get('/costs', [App\Http\Controllers\CostController::class, 'index'])->name('costs.index');
-Route::post('/costs', [App\Http\Controllers\CostController::class, 'store'])->name('costs.store');
-
 require __DIR__.'/auth.php';
